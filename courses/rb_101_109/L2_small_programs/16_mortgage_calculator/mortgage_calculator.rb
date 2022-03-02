9d1f8019f1dcf1523af2319cc7e2166d4e6005ca
Chris Magcamit <chris.magcamit@gmail.com>
2022-03-02 10:20:03 -0800

    mortgage_calculator initial commit

---
+++ courses/rb_101_109/L2_small_programs/16_mortgage_calculator/mortgage_calculator.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+# PSEUDO CODE
+# Given loan_amount in dollars
+# Given annual_percent_rate (APR) as a percent
+# Given loan_duration_years in years
+# SET loan_amount = make sure it is in dollars, convert to float type, save value.
+# SET monthly_percent_rate = convert annual_percent_rate to decimal, convert to monthly_percent_rate, save value.
+# SET loan_duration_months = convert to months, save value. 
+# 
+# FORMAL PSEUDO CODE
+# MACHINE CODE
