1f323b48f5a92acf3a5eead3ef2c4ebd438d8f52
Chris Magcamit <chris.magcamit@gmail.com>
2022-03-02 11:11:52 -0800

    mortgage_calculator barebones complete.

--- courses/rb_101_109/L2_small_programs/16_mortgage_calculator/mortgage_calculator.rb
+++ courses/rb_101_109/L2_small_programs/16_mortgage_calculator/mortgage_calculator.rb
@@ -1,4 +1,5 @@
 # frozen_string_literal: true
+
 # PSEUDO CODE
 # Given value 1= loan_amount (dollars), annual percent rate, and loan duration (years)
 # Convert value 2= annual percent rate to monthly percent rate
@@ -16,19 +17,27 @@
 # RETURN monthly_payment
 
 # MACHINE CODE
-#methods
+# methods
 def prompt(string)
   Kernel.puts("=> #{string}")
 end
 
-def get_input
-  Kernel.gets().chomp().to_f
+def usr_input
+  Kernel.gets.chomp.to_f
 end
 
 # Given loan_amount, annual_percent_rate, loan_duration_years
-loan_amount = get_input
-# SET loan_amount
+prompt("Please enter the loan amount in dollars ($).")
+ln_amount = usr_input
+prompt("Please enter the loan duration in years.")
+ln_duration_years = usr_input
+prompt("Please enter the annual percent rate (APR) (for example: 6 for 6%; 10 for 10%).")
+an_interest_rate = usr_input
 # SET monthly_percent_rate
+p mn_interest_rate = an_interest_rate / 100 / 12
 # SET loan_duration_months
+p ln_duration_months = ln_duration_years * 12
 # SET monthly_payment
+mn_payment = ln_amount * (mn_interest_rate / (1 - (1 + mn_interest_rate)**(-ln_duration_months)))
 # RETURN monthly_payment
+prompt("This is monthly payment $#{mn_payment.round(2)}.")
