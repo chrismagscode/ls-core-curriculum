af0dc43f35297c823f502b89ab5e9e52cd3fb4e3
Chris Magcamit <chris.magcamit@gmail.com>
2022-03-02 10:40:29 -0800

    mortgage_calculator update with pseudo code

--- courses/rb_101_109/L2_small_programs/16_mortgage_calculator/mortgage_calculator.rb
+++ courses/rb_101_109/L2_small_programs/16_mortgage_calculator/mortgage_calculator.rb
@@ -1,11 +1,34 @@
 # frozen_string_literal: true
 # PSEUDO CODE
-# Given loan_amount in dollars
-# Given annual_percent_rate (APR) as a percent
-# Given loan_duration_years in years
-# SET loan_amount = make sure it is in dollars, convert to float type, save value.
-# SET monthly_percent_rate = convert annual_percent_rate to decimal, convert to monthly_percent_rate, save value.
-# SET loan_duration_months = convert to months, save value. 
-# 
+# Given value 1= loan_amount (dollars), annual percent rate, and loan duration (years)
+# Convert value 2= annual percent rate to monthly percent rate
+# Convert value 3= loan duration in years to loan duration in months
+# Calculate monthly payment using value 1, value 2, and value 3
+# Return monthly payment
+
 # FORMAL PSEUDO CODE
+# Initialize variables loan_amount, annual_percent_rate, loan_duration_years
+# GET loan_amount, annual_percent_rate, loan_duration_years
+# SET loan_amount
+# SET monthly_percent_rate
+# SET loan_duration_months
+# SET monthly_payment
+# RETURN monthly_payment
+
 # MACHINE CODE
+#methods
+def prompt(string)
+  Kernel.puts("=> #{string}")
+end
+
+def get_input
+  Kernel.gets().chomp().to_f
+end
+
+# Given loan_amount, annual_percent_rate, loan_duration_years
+loan_amount = get_input
+# SET loan_amount
+# SET monthly_percent_rate
+# SET loan_duration_months
+# SET monthly_payment
+# RETURN monthly_payment
