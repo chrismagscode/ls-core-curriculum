db23533a2abe5c9fe9d16f5b88b54d3aecc9f5e6
Chris Magcamit <chris.magcamit@gmail.com>
2022-03-02 13:09:34 -0800

    all definitions defined

--- courses/rb_101_109/L2_small_programs/16_mortgage_calculator/mortgage_calculator.rb
+++ courses/rb_101_109/L2_small_programs/16_mortgage_calculator/mortgage_calculator.rb
@@ -17,30 +17,35 @@
 # RETURN monthly_payment
 
 # MACHINE CODE
-# methods
 def prompt(string)
   Kernel.puts("=> #{string}")
 end
 
 def usr_input
-  Kernel.gets.chomp.to_f
+  temp = nil
+  loop do
+    temp = Kernel.gets.chomp.to_f
+    break if temp.to_f > 0.0
+    prompt("Please enter an amount > 0.")
+  end
+  temp
 end
-
-# Given loan_amount, annual_percent_rate, loan_duration_years
-prompt("Please enter the loan amount in dollars ($).")
-ln_amount = nil
-loop do
-  break if ln_amount = usr_input > 0
+def mortgage_calculator
+  prompt("Please enter the loan amount in dollars ($).")
+  ln_amount = nil
+  loop do
+    ln_amount = usr_input.to_i
+    break if ln_amount
+    prompt("Please enter an amount greater than $0.")
+  end
+  prompt("Loan amount: $#{ln_amount}.")
+  prompt("Please enter the loan duration in years.")
+  ln_duration_years = usr_input
+  prompt("Please enter the annual interest rate (APR).")
+  prompt("For example: 6 is 6%; 10 is 10%).")
+  an_interest_rate = usr_input
+  p mn_interest_rate = an_interest_rate / 100 / 12
+  p ln_duration_months = ln_duration_years * 12
+  mn_payment = ln_amount * (mn_interest_rate / (1 - (1 + mn_interest_rate)**(-ln_duration_months)))
+  prompt("Your monthly payment is: $#{format('%.2f', mn_payment)}")
 end
-prompt("Please enter the loan duration in years.")
-ln_duration_years = usr_input
-prompt("Please enter the annual percent rate (APR) (for example: 6 for 6%; 10 for 10%).")
-an_interest_rate = usr_input
-# SET monthly_percent_rate
-p mn_interest_rate = an_interest_rate / 100 / 12
-# SET loan_duration_months
-p ln_duration_months = ln_duration_years * 12
-# SET monthly_payment
-mn_payment = ln_amount * (mn_interest_rate / (1 - (1 + mn_interest_rate)**(-ln_duration_months)))
-# RETURN monthly_payment
-prompt("This is monthly payment $#{mn_payment.round(2)}.")
