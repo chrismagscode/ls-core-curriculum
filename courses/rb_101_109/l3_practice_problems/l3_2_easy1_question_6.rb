a3a2c1eed8f056b3e4cb2c93c9ba218bdb873a4b
Chris Magcamit <chris.magcamit@gmail.com>
2022-04-11 13:28:59 -0700

    Problem completed with 3 different methods.

---
+++ courses/rb_101_109/l3_practice_problems/l3_2_easy1_question_6.rb
@@ -0,0 +1,10 @@
+famous_words = "seven years ago..."
+
+# method 1
+"Four score and " + famous_words
+
+# # method 2
+# famous_words.prepend("Four score and ")
+
+# # method 3
+# "Four score and " << famous_words
