287b7fd14e28372ce2a05d862bc3938b7b847b69
Chris Magcamit <chris.magcamit@gmail.com>
2022-04-11 13:25:58 -0700

    Initial commit.

---
+++ courses/rb_101_109/l3_practice_problems/l3_2_easy1_question_5.rb
@@ -0,0 +1,8 @@
+# Programmatically determine if 42 lies between 10 and 100.
+
+# determine if 42 lies between 10 and 100
+
+# ruby range lists ranges from x to y
+# range returns in array format
+
+(10..100).cover?(42)
