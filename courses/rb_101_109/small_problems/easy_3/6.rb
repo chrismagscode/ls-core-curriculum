a995f928b2b61bd6527a92b4008430ace9313018
Chris Magcamit <chris.magcamit@gmail.com>
2022-06-30 17:51:29 -0700

    Initial commit.

--- courses/rb_101_109/small_problems/easy_3/6.rb
+++ courses/rb_101_109/small_problems/easy_3/6.rb
@@ -1,19 +1,16 @@
 # Exclusive Or
 
 def xor?(value1, value2)
-  !! ((value1 && !value2) || (value2 && !value1))
+  return true if value1 && !value2
+  return true if value2 && !value1
+end
+
+# solution_2
+def xor?(value1, value2)
+  !!((value1 && !value2) || (value2 && !value1))
 end
 
 xor?(5.even?, 4.even?) == true
 xor?(5.odd?, 4.odd?) == true
 xor?(5.odd?, 4.even?) == false
 xor?(5.even?, 4.odd?) == false
-
-# light switch example
-
-def three_way_switch(switch1, switch2)
-  !! ((switch1 == 'up' && switch2 == 'down' || switch2 == 'up' && switch1 == 'down'))
-end
-
-
-p three_way_switch('up', 'up')
